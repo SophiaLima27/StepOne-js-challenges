760c1f687ecdb48a5303d54d4fc56eb50093812c
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:12:08 -0300

    Update 13-string.js

--- 13-string.js
+++ 13-string.js
@@ -7,8 +7,13 @@
 // `Maria Silva` => `M.S.`
 // `Pedro Pedroso`  => `P.P`
 
-function abbrevName(firstName, lastName){
-	
+let firstName = "Sophia";
+let middleName = "Lima";
+let lastName = "Trevisan";
 
+function abbrevName(firstName, middleName, lastName) {
+	console.log (firstName.charAt (0) + '. ' + middleName.charAt (0) + '. ' + lastName.charAt (0) + '. ');
 	return;
 }
+
+abbrevName(firstName, middleName, lastName);

67e1dece95dc19afd37b5eafe9b53da2b581f881
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:12:26 -0300

    Update 14-while.js

--- 14-while.js
+++ 14-while.js
@@ -3,3 +3,23 @@
 // Utilizando o while, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
 
 // Utilizando o while, faça uma estrutura de repetição que dê o console dos números 100 a 1.
+
+let t = 50;
+let s = 2;
+let l = 100; 
+
+while (t<= 100) {
+    console.log (t);
+    t++;
+}
+
+
+while (s<= 100) {
+    console.log (s);
+    s = s+ 2
+}
+
+while (l>=1) {
+    console.log (l);
+    l--;
+}

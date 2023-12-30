9c301afc2c32287877150fee5a17519755b7181d
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:12:42 -0300

    Update 15-for.js

--- 15-for.js
+++ 15-for.js
@@ -3,3 +3,15 @@
 // Utilizando o for, faça uma estrutura de repetição que dê o console somente dos números pares, começando no 2 e indo até 100.
 
 // Utilizando o for, faça uma estrutura de repetição que dê o console dos números 100 a 1.
+
+for (let i = 50; i<=100;i++) {
+    console.log (i);
+}
+
+for (let s=2; s<=100; s = s + 2) {
+    console.log (s);
+}
+
+for (let t = 100; t>=1; t--) {
+    console.log (t);
+}

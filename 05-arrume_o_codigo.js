5d44b37f8f1aeb68d988c27fa65ad3f76d03bb98
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:07:26 -0300

    Update 05-arrume_o_codigo.js

--- 05-arrume_o_codigo.js
+++ 05-arrume_o_codigo.js
@@ -7,3 +7,15 @@ let result == a + b;
 console.log("Seu resultado é: " + result);
 
 console.log("O código tá certo? " + (result == "Não existe sucesso sem esforço") );
+
+//código arrumado:
+
+let a = "Não existe sucesso";
+
+let b = " sem esforço";
+
+let result = a + b;
+
+console.log("Seu resultado é: " + result);
+
+console.log("O código tá certo? " + (result == "Não existe sucesso sem esforço") );

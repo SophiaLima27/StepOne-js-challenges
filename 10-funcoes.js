ead4c87d2aef95a943161ad7a60f60b611a5d930
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:11:00 -0300

    Update 10-funcoes.js

--- 10-funcoes.js
+++ 10-funcoes.js
@@ -5,3 +5,13 @@
 // A função deve mostrar no console a área do retângulo com a seguinte frase:
 // The area of rectangle is XXX.
 
+
+function rectangleArea (length, width) {
+    const area = length * width;
+    return area;
+}
+
+console.log ("The area of rectangle is " + rectangleArea(5, 7) + " m².");
+
+let areaOfRectangle = rectangleArea (3, 5);
+console.log ("The area of rectangle is " + rectangleArea(3, 5) + " m².");

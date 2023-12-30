928735e2e8e669e24fbc5076b408e4f3bce135db
Sophia de Lima Trevisan <139834488+SophiaLima27@users.noreply.github.com>
2023-12-30 18:10:45 -0300

    Update 09-funcoes.js

--- 09-funcoes.js
+++ 09-funcoes.js
@@ -2,19 +2,43 @@
 
 // 1- Função de soma, com o nome ‘sum', que retorna a soma de dois números.
 
+function sum(a, b) {
+    const addition = a + b;
+    return addition;
+}
 
-
+let sumOfNumbers = sum(28, 26);
+console.log ("The result of sum is " + sumOfNumbers);
 
 // 2- Função de subtração, com o nome ‘subtract’, que retorna a subtração de dois números
 
+function subtract (a, b) {
+    const subtract = a - b;
+    return subtract;
+}
 
+let subtractOfNumbers = subtract (28, 26);
+console.log ("The result of subtract is " + subtractOfNumbers);
 
 // 3- Função de multiplicação, com o nome ‘multiplication, que retorna 
 // a multiplicação de dois números
 
+function multiplication (a, b) {
+    const multiplication = a * b;
+    return multiplication;
+}
 
+let multiplicationOfNumbers = multiplication (28, 26);
+console.log ("The result of multiplication is " + multiplicationOfNumbers);
 
+  
 // 4- Função de divisão, com o nome 'division', que retorna a divisão do primeiro 
 //número pelo outro
 
+function division (a, b) {
+    const division = a/b;
+    return division;
+}
 
+let divisionOfNumbers = division (28, 26);
+console.log ("The result of division is " + divisionOfNumbers);
